29e0c38d155058012fad4c9c04dcac96c57170da
xusumu <xusumu@gmail.com>
2020-08-27 14:14:01 +0800

    lists in react

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-const title = 'React';
+const list = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ]; 
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    // <div className="App">
+    //   <h1> hc stories </h1>
+    //   <label htmlFor="search">Search: </label> 
+    //   <input id="search" type="text" /> 
+    //   <hr /> 
+    //   {list.map(function(item) { return ( <div key={item.objectID}> <span> <a href={item.url}>{item.title}</a> </span> <span>{item.author}</span> <span>{item.num_comments}</span> <span>{item.points}</span> </div> ); })} 
+    // </div>
+    null
   );
 }
 

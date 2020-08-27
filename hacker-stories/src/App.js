74da27deccfad3b9af6af596ce529202c29c2e63
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-08-27 18:52:46 +0800

    Update App.js

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
 // import './App.css';
 
-const title = 'React';
+const title = "React";
+
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      {/* <h1>hello world</h1> */}
+      <h1>hello {title}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
